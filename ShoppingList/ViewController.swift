30228d9fc05c79375a9281adc9d5fe8c3912a77d
Andrei Korikov <kpanic666@gmail.com>
2021-10-20 14:40:54 +0500

    All main parts of app is done

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -7,13 +7,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var shoppingList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addToolBarItems()
+        addNavBarItems()
+    }
+    
+    fileprivate func addToolBarItems() {
+        let addItemAction = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptNewItem))
+        addItemAction.tintColor = UIColor.systemPink
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolbarItems = [spacer, addItemAction, spacer]
+    }
+    
+    func addNavBarItems() {
+        let clearAction = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(clearShoppingList))
+        clearAction.tintColor = UIColor.systemPink
+        
+        let sharingAction = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareActivityWindow))
+        sharingAction.tintColor = UIColor.systemPink
+        
+        navigationItem.leftBarButtonItem = clearAction
+        navigationItem.rightBarButtonItem = sharingAction
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShopItem", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    @objc func shareActivityWindow() {
+        let ac = UIActivityViewController(
+            activityItems: [shoppingList.joined(separator: "\n")],
+            applicationActivities: nil
+        )
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
     }
 
-
+    @objc func promptNewItem() {
+        let ac = UIAlertController(title: "New Shopping Item", message: "Please, enter a new item:", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let confirmAction = UIAlertAction(title: "OK", style: .default) { [unowned ac, weak self] _ in
+            guard let item = ac.textFields?[0].text else { return }
+            if !item.isEmpty {
+                self?.addNewItem(item)
+            }
+        }
+        ac.addAction(confirmAction)
+        present(ac, animated: true)
+    }
+    
+    func addNewItem(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
+    
+    @objc func clearShoppingList() {
+        shoppingList.removeAll()
+        tableView.reloadData()
+    }
 }
 
